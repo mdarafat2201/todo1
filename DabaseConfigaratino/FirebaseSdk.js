53df0699cbbbe1a93fab23944c2ad8c50b36478b
mdarafat2201 <maccenarafat@gmail.com>
2024-02-18 07:45:11 +0000

    first todo

---
+++ DabaseConfigaratino/FirebaseSdk.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBXFcfQ67HOdJUVlorAroAt7VtK0YTlW_0",
+  authDomain: "to-do-6ad96.firebaseapp.com",
+  projectId: "to-do-6ad96",
+  storageBucket: "to-do-6ad96.appspot.com",
+  messagingSenderId: "146026572887",
+  appId: "1:146026572887:web:96fc837f8bb5081a1a4712",
+  measurementId: "G-S2L59MD61P",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
