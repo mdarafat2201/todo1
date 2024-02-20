0efea159d817771a1b84e59131db3b9b3b55e7eb
mdarafat2201 <maccenarafat@gmail.com>
2024-02-20 14:43:31 +0000

    todo4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getDatabase, ref, set } from "firebase/database";
+import "./App.css";
 function App() {
   const db = getDatabase();
-  useEffect(() => {
-    let databaseName = ref(db, "todo/");
-    set(databaseName, {
-      username: "arafat mai",
-      email: "maccenarafat@gmail.com",
-    });
-  }, []);
+
+  // all state
+  const [todoInput, settodoInput] = useState("");
+
+  //HandleInout function
+  const HandleInout = (e) => {
+    e.preventDefault();
+    if ((todoInput |= "")) {
+      const dbinfo = ref(db, "todo/");
+      set(dbinfo, {
+        todoItem: todoInput,
+      })
+        .then(() => {
+          console.log("upload sucessfully");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  console.log(todoInput);
   return (
     <>
-      <h2>kmlkdmklm</h2>
+      <div className="todobody">
+        <form>
+          <input type="text" className="inputfield" onChange={HandleInout} />
+          <button className="addbtn">Add</button>
+        </form>
+      </div>
     </>
   );
 }
